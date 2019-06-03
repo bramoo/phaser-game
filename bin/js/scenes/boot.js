539073f6f1a53eec885d6f6d0d071459765791a9
bramoo <brad.moorfield@gmail.com>
2019-06-03 22:15:50 +1200

    Add player

--- bin/js/scenes/boot.js
+++ bin/js/scenes/boot.js
@@ -6,11 +6,11 @@ export class Boot extends Phaser.Scene {
     }
 
     preload() {
-        this.load.setBaseURL('http://labs.phaser.io');
-
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+        this.load.image('sky', 'assets/space3.png');
+        this.load.image('logo', 'assets/phaser3-logo.png');
+        this.load.image('red', 'assets/red.png');
+        this.load.image('platform', 'assets/platform.png');
+        this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 64, frameHeight: 64});
 
         this.load.webfont({
             google: {
@@ -45,10 +45,55 @@ export class Boot extends Phaser.Scene {
 
         let title = this.add.text(0, 0, "GAME", {fontFamily: '"Bungee Outline"'});
         this.fillText(title);
+
+        this.player = this.physics.add.sprite(10, 10, 'dude');
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'run',
+            frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 5}),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'stop',
+            frames: [{key: 'dude', frame: 6}]
+        });
+
+        this.anims.create({
+            key: 'fall',
+            frames: [{key: 'dude', frame: 7}]
+        });
+
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 480, 'platform', null, false);
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update(time, delta){
         this.crtPipeline.setFloat1('time', time / 1000);
+        let grounded = this.player.body.touching.down;
+
+        if (this.cursors.left.isDown){
+            this.player.setVelocityX(-160);
+            this.player.anims.play(grounded ? 'run' : 'fall', true);
+            this.player.flipX = false;
+        } else if (this.cursors.right.isDown){
+            this.player.setVelocityX(160);
+            this.player.anims.play(grounded ? 'run' : 'fall', true);
+            this.player.flipX = true;
+        } else {
+            this.player.setVelocityX(0);
+            this.player.anims.play(grounded ? 'stop' : 'fall', true);
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down){
+            this.player.setVelocityY(-330);
+        }
     }
 
     fillText(title) {
